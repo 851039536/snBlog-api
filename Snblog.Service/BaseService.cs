52a3f56ff904a023c72b14f35dbdfca4d82e96bd
851039536 <851039536@qq.com>
2023-05-21 20:37:17 +0800

    fix:更正命名方式

--- Snblog.Service/BaseService.cs
+++ Snblog.Service/BaseService.cs
@@ -5,17 +5,17 @@ namespace Snblog.Service
     public class BaseService : IBaseService
     {
         private readonly IRepositoryFactory _repositoryFactory;
-        private readonly IConcardContext _mydbcontext;
+        private readonly IConcardContext _myDbcontext;
 
-        protected BaseService(IRepositoryFactory repositoryFactory, IConcardContext mydbcontext)
+        protected BaseService(IRepositoryFactory repositoryFactory, IConcardContext myDbcontext)
         {
             _repositoryFactory = repositoryFactory;
-            _mydbcontext = mydbcontext;
+            _myDbcontext = myDbcontext;
         }
 
         public IRepositorys<T> CreateService<T>() where T : class, new()
         {
-            return _repositoryFactory.CreateRepository<T>(_mydbcontext);
+            return _repositoryFactory.CreateRepository<T>(_myDbcontext);
         }
     }
 }
